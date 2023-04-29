af645bb8a2866c9ad913e6d61006f2a614c7cac3
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-04-28 22:35:22 -0300

    Feature: arquivo main adicionado

---
+++ main.py
@@ -0,0 +1,3 @@
+# Vamos iniciar o Trabalho aki.
+# Tomem cuidado para mexer só na parte de vocês para não quebrar o programa
+# Usem branchs para codar e depois façam o merge, não tem muito segredo.
