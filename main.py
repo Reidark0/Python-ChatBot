b1510a5107c9f15ce1caed739fcc4d230f03605f
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-04-28 22:41:34 -0300

    Edit: comentários aumentados e início do código para referencia

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Vamos iniciar o Trabalho aki.
 # Tomem cuidado para mexer só na parte de vocês para não quebrar o programa
 # Usem branchs para codar e depois façam o merge, não tem muito segredo.
+# 
+# Nos Commits escrevam no início:
+# "feature: " ao adicionar coisas
+# "edit: " se editarem alguma coisa, sem adicionar nada
+# "fix: se consetarem algum problema no código "
+# e descrevam oque você fez.
+
+print("Bem vindo ao ChatBot da Barbearia S9!")
+print("Como podemos te ajudar?")
