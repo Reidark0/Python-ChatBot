93ae90f06000ca58cd15282eb8db785504cf564f
Rafael Cesar <rafa.cesar.vet@gmail.com>
2023-04-28 22:45:28 -0300

    edit: mais comentarios no ínicio

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# Vamos iniciar o Trabalho aki.
+# Vamos iniciar o Trabalho aqui.
 # Tomem cuidado para mexer só na parte de vocês para não quebrar o programa
-# Usem branchs para codar e depois façam o merge, não tem muito segredo.
+# Se não forem mexer sozinhos usem branchs para codar e depois façam o merge.
+# Não economizem nos commits, pode fazer vários sem medo.
 # 
 # Nos Commits escrevam no início:
 # "feature: " ao adicionar coisas
 # "edit: " se editarem alguma coisa, sem adicionar nada
 # "fix: se consetarem algum problema no código "
+# "Style: " se a mudança for na aparencia do código
 # e descrevam oque você fez.
+# 
+# # Lembrem de Dar git pull antes de começar a codar pra editar o código mais recente
 
 print("Bem vindo ao ChatBot da Barbearia S9!")
 print("Como podemos te ajudar?")
